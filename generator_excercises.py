1cbf6e528673619c0b9df2757af5e873ebd93f4e
lekhuv <lekhu.venkat@gmail.com>
2022-06-27 14:45:44 -0700

    Lambda, generation problems

--- generator_excercises.py
+++ generator_excercises.py
@@ -2,9 +2,20 @@
 
 #Problem 1
 #Create a generator, primes_gen that generates prime numbers starting from 2.
-
+def primes_gen():
+    n = 2
+    primes = set()
+    while True:
+        for p in primes:
+            if n % p == 0:
+                break
+        else:
+            primes.add(n)
+            yield n
+        n += 1
+                
 gen = primes_gen()
-for _ in range(10):
+for n in range(10):
     print(next(gen), end=' ')
 # Expected output
 # 2 3 5 7 11 13 17 19 23 29
@@ -14,6 +25,8 @@ for _ in range(10):
 #Create a generator, unique_letters that generates unique letters from
 #the input string. It should generate the letters in the same order as
 #from the input string.
+
+
 
 for letter in unique_letters('hello'):
     print(letter, end=' ')
--- lambda_excercise.py
+++ lambda_excercise.py
@@ -11,18 +11,35 @@ prog_lang = [('Python', 3.8),
 #1. Sort the list by each language's version in ascending order.
 #[('Scala', 2.13), ('Python', 3.8), ('Java', 13), ('JavaScript', 2019)]
 
+print(sorted(prog_lang, key=lambda course: course[1]))
+
 #2. Sort the list by the length of the name of each language in descending order.
 #[('JavaScript', 2019), ('Python', 3.8), ('Scala', 2.13), ('Java', 13)]
+
+prog_lang.sort(key = lambda x: len(x[0]), reverse = True)
+print(prog_lang)
 
 #3. Filter the list so that it only contains languages with 'a' in it.
 #[('Java', 13), ('JavaScript', 2019), ('Scala', 2.13)]
 
+def filter_a(prog_lang):
+    return list(filter(lambda x: 'a' in x[0], prog_lang))
+print(filter_a(prog_lang))  
+
 #4. Filter the list so that it only contains languages whose version is in integer form.
 #[('Java', 13), ('JavaScript', 2019)]
+
+def filter_int(prog_lang):
+    list1 = []
+    list1 =  list(filter(lambda x: type(x[1] == int, prog_lang)))
+    return list1    
+print(prog_lang)
 
 #5. Transform the list so that it contains the tuples in the form,
 #("language in all lower case", length of the language string)
 #[('python', 6), ('java', 4), ('javascript', 10), ('scala', 5)]
+
+
 
 #6. Generate a tuple in the form,
 #("All languages separated by commas","All versions separated by commas")
